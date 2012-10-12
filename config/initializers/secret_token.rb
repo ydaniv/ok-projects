6c9945487a84ab7b69bfeb0cde2e161a27c68d49
Zohar Arad <zohar@zohararad.com>
2012-10-12 12:50:40 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OkProjects::Application.config.secret_token = 'c5fed836c55a28c48454d4ae9ee42124902fe00115647e0226b1812d0713e794a93add385eecc50b32874b3ab86a7a7b37a06f9179b67931a279a51fb4ecac13'
