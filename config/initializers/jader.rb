5670924602e3df58da7000ff186536c410319b54
Zohar Arad <zohar@zohararad.com>
2012-10-12 13:51:18 +0200

    add jader initializer

---
+++ config/initializers/jader.rb
@@ -0,0 +1,4 @@
+Jader.configure do |config|
+  config.views_path = Rails.root.join('app','assets','javascripts','app','views')
+  config.mixins_path = Rails.root.join('app','assets','javascripts','app','mixins')
+end
